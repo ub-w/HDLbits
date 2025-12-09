bdb15d343f5c0110ca84e24ea0dfc016a11c579e
ub-w <1820234756@qq.com>
2025-12-09 16:16:53 +0800

    update

---
+++ P02Circuits/c01Combinational Logic/s01Basic Gates/Combine circuits A and B.v
@@ -0,0 +1,16 @@
+module top_module (input x, input y, output z);
+    wire z1,z2,z3,z4;
+    A inist1(x,y,z1);
+    B inist2(x,y,z2);
+    A inist3(x,y,z3);
+    B inist4(x,y,z4);
+    assign z = (z1|z2)^(z3&z4);
+endmodule
+
+module A(input x, input y, output z);
+    assign z = (x^y)&x;
+endmodule
+
+module B( input x, input y, output z );
+    assign z = (~x&~y)|(x&y);
+endmodule
